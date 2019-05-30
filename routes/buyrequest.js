3dcd126e608d4473b558692dfabb2624ce8760c0
Yang-Seong-Young <2014112096@dongguk.edu>
2019-05-30 22:11:51 +0900

    오류수정좀

--- routes/buyrequest.js
+++ routes/buyrequest.js
@@ -4,69 +4,82 @@ module.exports = function (app) {
 	var router = express.Router();
 	var mysqlDB = require('../config/db');
 
-    var invoke = require('../blockchain/invoke-transaction.js');
-    var query = require('../blockchain/query.js');
+	var invoke = require('../blockchain/invoke-transaction.js');
+	var query = require('../blockchain/query.js');
 	var dna = require('../config/dna');
-    
-    var peer = dna.peer;
-    var channelName = dna.channelName;
-    var chaincodeName = dna.chaincodeName;
-    var username = dna.username;
-    var orgname = dna.orgname;
-    
-    var readpid = function(query){
-		return new Promise(function(resolve, reject){
-			mysqlDB.query(query,  function(err, rows, fields ){
-				var pd_id= rows.length+1;
+
+	var peer = dna.peer;
+	var channelName = dna.channelName;
+	var chaincodeName = dna.chaincodeName;
+	var username = dna.username;
+	var orgname = dna.orgname;
+
+	var readpid = function (query) {
+		return new Promise(function (resolve, reject) {
+			mysqlDB.query(query, function (err, rows, fields) {
+				var pd_id = rows.length + 1;
 				resolve(pd_id);
 			});
 		})
 	}
-	var readDB = function(query){
-		return new Promise(function(resolve, reject){
-			mysqlDB.query(query,  function(err, rows, fields ){
+	var readDB = function (query) {
+		return new Promise(function (resolve, reject) {
+			mysqlDB.query(query, function (err, rows, fields) {
 				resolve(rows);
 			});
 		})
-    }
-    
+	}
+
 	router.get('/', async function (req, res) {
-		console.log(req.query.pid);
-        var query = 'SELECT * FROM newbabodb.Product WHERE Product_id = '+ req.query.pid+';';
-        var query2 = 'insert into newbabodb.Order value(?,?,?,?,?,?)' 
-        var query3 = 'UPDATE newbabodb.Product SET status=1 WHERE Product_id = '+req.query.pid+';';
-        var order_id = await readpid('SELECT * FROM newbabodb.Order;');
-        var buyer_id = req.session.userID;
-        console.log(buyer_id);
-        mysqlDB.query(query3, async function(err,rows,fields){
-            if(err){
-				console.log('query error :'+err);
-			}else{
-				console.log(rows);
-				var thisprod = await readDB(query);
-				console.log(thisprod);
-				res.redirect('/product?pid='+req.query.pid);
-                mysqlDB.query(query2, [0, req.session.userID, req.query.pid, 1, 0,order_id], async function(err,rows_, fields){
-                    if(err){
-                        console.log('query error :'+err);
-                    }else{
-						//console.log(rows_);
-						
-						try {
-							var fcn = 'tx_state';
-							//	  0	      1        2		  3 		  4        5         6	        7		 8	     9
-							//	txID  txState  sellerID  sellerName  sellerRRN  buyerID  buyerName  buyerRRN  product  price
-							var args = [req.query.pid, 'match', , req.session.userID, req.session.usernam, ];
-							await invoke.invokeChaincode(peer, channelName, chaincodeName, fcn, args, username, orgname);
-						} catch (err) {
-							console.log(err);
-						}
-                    }
-                })
-            }
-          
-        })
-        
+		var buyerID = req.session.userID;
+		var query = 'select id, Member_name, RRN_hash, Product_name, Product_price\
+					from newbabodb.Member, newbabodb.Product\
+					where id =(select Member_id from newbabodb.Order where Product_id = ' + req.query.pid + ') and Product_id = ' + req.query.pid + '\
+					union\
+					select id, Member_name,RRN_hash, Product_name, Product_price\
+					from newbabodb.Member, newbabodb.Product\
+					where id =(select Member_id from newbabodb.Product where Product_id = ' + req.query.pid + ') and Product_id = ' + req.query.pid;
+		var query2 = 'insert into newbabodb.Order value(?,?,?,?,?,?)'
+		var query3 = 'UPDATE newbabodb.Product SET status=1 WHERE Product_id = ' + req.query.pid + ';';
+		var order_id = await readpid('SELECT * FROM newbabodb.Order;');
+		
+		
+
+		mysqlDB.query(query3, async function (err, rows__, fields) {
+			if (err) {
+				console.log('query error :' + err);
+			} else {
+				res.redirect('/product?pid=' + req.query.pid);
+
+				mysqlDB.query(query2, [0, buyerID, req.query.pid, 1, 0, order_id], function (err, rows_, fields) {
+					if (err) {
+						console.log('query error :' + err);
+					} else {
+
+						mysqlDB.query(query, async function (err, rows, fields) {
+							if (err) {
+								console.log('query error :' + err);
+							} else {
+								console.log(rows)
+								try {
+									var fcn = 'tx_state';
+									//	  0	      1        2		  3 		  4        5         6	        7		 8	     9
+									//	txID  txState  sellerID  sellerName  sellerRRN  buyerID  buyerName  buyerRRN  product  price
+									var args = [order_id.toString(), 'match', rows[0].id, rows[0].Member_name, rows[0].RRN_hash, rows[1].id, rows[1].Member_name, rows[1].RRN_hash, rows[1].Product_name, rows[1].Product_price.toString()];
+									await invoke.invokeChaincode(peer, channelName, chaincodeName, fcn, args, username, orgname);
+								} catch (err) {
+									console.log('invoke error :' + err);
+								}
+									
+							}
+
+						})
+					}
+				})
+			}
+
+		})
+
 	});
 
 
--- routes/test.js
+++ routes/test.js
@@ -1,102 +1,81 @@
 module.exports = function (app) {
 
-	var express = require('express');
+    var express = require('express');
     var router = express.Router();
+    var mysqlDB = require('../config/db');
 
     var invoke = require('../blockchain/invoke-transaction.js');
     var query = require('../blockchain/query.js');
-	var dna = require('../config/dna');
-    
+    var dna = require('../config/dna');
+
     var peer = dna.peer;
     var channelName = dna.channelName;
     var chaincodeName = dna.chaincodeName;
     var username = dna.username;
     var orgname = dna.orgname;
-    
+
     router.get('/', function (req, res) {
         res.status(200);
-        
+
         res.render('test', {
-			login: req.session.login,
-			userid: req.session.userID,
-			username: req.session.username,
-			authority: req.session.authority,
-			page: 'main'
-		});
+            login: req.session.login,
+            userid: req.session.userID,
+            username: req.session.username,
+            authority: req.session.authority,
+            page: 'main'
+        });
     });
 
     router.get('/history/:args', async function (req, res) {
         res.status(200);
-        
+
         var _type = req.query.type;
         var _results_json = new Array();
-        var args = [req.params.args];
-        var fcn = 'queryBySeller';
-        var _result;
+        var queryString = "SELECT RRN_hash FROM Member WHERE id = " + "'" + req.params.args + "'";
+        
+        mysqlDB.query(queryString, async function (err, rows, fields) {
+            if (err) {
+                console.log('query error :' + err);
+            } else {
+                var args = [rows];
+                var fcn = 'queryBySeller';
+                var _result;
 
-        try {
-            _result = await query.queryChaincode(peer, channelName, chaincodeName, args, fcn, username, orgname);
-            if (_result == '') { }
-            else {
-                var _results = _result.split('&&');
-                for (var i = 0; i < _results.length; i++) {
-                    _results_json.push(JSON.parse(_results[i]));
+                try {
+                    _result = await query.queryChaincode(peer, channelName, chaincodeName, args, fcn, username, orgname);
+                    if (_result == '') { }
+                    else {
+                        var _results = _result.split('&&');
+                        for (var i = 0; i < _results.length; i++) {
+                            _results_json.push(JSON.parse(_results[i]));
+                        }
+                    }
+                } catch (err) {
+                    console.log(err);
                 }
             }
-        } catch (err) {
-            console.log(err);
-        }
-        
+        })
+
+
         res.render('history', {
-			login: req.session.login,
-			userid: req.session.userID,
-			username: req.session.username,
-			authority: req.session.authority,
+            login: req.session.login,
+            userid: req.session.userID,
+            username: req.session.username,
+            authority: req.session.authority,
             page: 'null',
             result: _results_json,
-            sellerID: args,
+            sellerID: req.query.args,
             type: _type
-		});
+        });
     });
-    
+
     router.get('/channels/:channelName/chaincodes/:chaincodeName', async function (req, res) {
-            var peer = req.query.peer;
-            var chaincodeName = req.params.chaincodeName;
-            var channelName = req.params.channelName;
-            var fcn = req.query.fcn;
-            var args = req.query.args;
-    
-            if (!chaincodeName) {
-                    res.json(getErrorMessage('\'chaincodeName\''));
-                    return;
-            }
-            if (!channelName) {
-                    res.json(getErrorMessage('\'channelName\''));
-                    return;
-            }
-            if (!fcn) {
-                    res.json(getErrorMessage('\'fcn\''));
-                    return;
-            }
-            if (!args) {
-                    res.json(getErrorMessage('\'args\''));
-                    return;
-            }
-    
-            args = args.replace(/'/g, '"');
-            args = JSON.parse(args);
-    
-            let message = await query.queryChaincode(peer, channelName, chaincodeName, args, fcn, username, orgname);
-            res.send({msg:message});
-    });
-    
-    router.post('/channels/:channelName/chaincodes/:chaincodeName', async function (req, res) {
-        var peer = req.body.peer;
+        var peer = req.query.peer;
         var chaincodeName = req.params.chaincodeName;
         var channelName = req.params.channelName;
-        var fcn = req.body.fcn;
-        var args = req.body.args;
-    
+        var fcn = req.query.fcn;
+        var args = req.query.args;
+
         if (!chaincodeName) {
             res.json(getErrorMessage('\'chaincodeName\''));
             return;
@@ -113,26 +92,57 @@ module.exports = function (app) {
             res.json(getErrorMessage('\'args\''));
             return;
         }
-    
+
+        args = args.replace(/'/g, '"');
+        args = JSON.parse(args);
+
+        let message = await query.queryChaincode(peer, channelName, chaincodeName, args, fcn, username, orgname);
+        res.send({ msg: message });
+    });
+
+    router.post('/channels/:channelName/chaincodes/:chaincodeName', async function (req, res) {
+        var peer = req.body.peer;
+        var chaincodeName = req.params.chaincodeName;
+        var channelName = req.params.channelName;
+        var fcn = req.body.fcn;
+        var args = req.body.args;
+
+        if (!chaincodeName) {
+            res.json(getErrorMessage('\'chaincodeName\''));
+            return;
+        }
+        if (!channelName) {
+            res.json(getErrorMessage('\'channelName\''));
+            return;
+        }
+        if (!fcn) {
+            res.json(getErrorMessage('\'fcn\''));
+            return;
+        }
+        if (!args) {
+            res.json(getErrorMessage('\'args\''));
+            return;
+        }
+
         let message
         args = args.replace(/'/g, '"');
         args = JSON.parse(args);
-    
-        if(fcn == "query") {	
+
+        if (fcn == "query") {
             message = await query.queryChaincode(peer, channelName, chaincodeName, args, fcn, username, orgname);
         }
-        else if(fcn == "queryByUserID") {
+        else if (fcn == "queryByUserID") {
             message = await query.queryChaincode(peer, channelName, chaincodeName, args, fcn, username, orgname);
         }
-        else if(fcn == "history") {
+        else if (fcn == "history") {
             message = await query.queryChaincode(peer, channelName, chaincodeName, args, fcn, username, orgname);
         }
-        else if(fcn == "tx_state") {
+        else if (fcn == "tx_state") {
             message = await invoke.invokeChaincode(peer, channelName, chaincodeName, fcn, args, username, orgname);
         }
-        
-        res.send({msg:message});
-    });    
 
-	return router;
+        res.send({ msg: message });
+    });
+
+    return router;
 }
