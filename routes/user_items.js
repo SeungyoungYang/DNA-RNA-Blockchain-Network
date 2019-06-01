a2a4a31f61b32264c8c52ade3e5ea3424f0d167d
jhyang2362 <jhyang2362@naver.com>
2019-06-02 01:59:30 +0900

    my request 수정 완료

--- routes/user_items.js
+++ routes/user_items.js
@@ -35,13 +35,24 @@ module.exports = function (app) {
 	});
 
 	router.get('/cancel', async function (req, res){
-		var cancelquery = 'DELETE FROM newbabodb.Order WHERE Number='+'\''+req.query.num+'\'';
-		var cancelquery2 = 'UPDATE newbabodb.Product SET status=0 WHERE Product_id='+'\''+req.query.item+'\'';
-		console.log(cancelquery);
-		console.log(cancelquery2);
+		var cancelquery = 'DELETE FROM newbabodb.Order WHERE Number='+'\''+req.query.num+'\';';
+		var cancelquery2 = 'UPDATE newbabodb.Product SET status=0 WHERE Product_id='+'\''+req.query.item+'\';';
 		await readDB(cancelquery);
 		await readDB(cancelquery2);
 		res.redirect('/user_items');
+	});
+
+	router.post('/invoice', async function(req, res){
+		var invoice = req.body['invoice'];
+		var number = req.body['number'];
+		var pd_id = req.body['pd_id'];
+		var invoicequery = 'UPDATE newbabodb.Order SET invoice_number='+'\''+invoice+'\'';
+		invoicequery += ' WHERE Number='+'\''+number+'\';';
+		var changestatusquery = 'UPDATE newbabodb.Product SET status=2 WHERE Product_id='+'\''+pd_id+'\';';
+		await readDB(invoicequery);
+		await readDB(changestatusquery);
+		res.redirect('/user_items');
 	})
+
 	return router;
 }
